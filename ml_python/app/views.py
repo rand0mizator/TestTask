f87ab7f982131c38d13ccba1f2f2b6697dce4f12
Rand0mizator <iosuslov@gmail.com>
2021-09-28 17:17:54 +0700

    Определение каких пикселей больше, белых или черных Подсчет пикселей введенного цвета

--- ml_python/app/views.py
+++ ml_python/app/views.py
@@ -3,10 +3,16 @@ from app.forms import ImageForm
 
 import cv2
 import numpy as np
+from PIL import ImageColor
 
 
 def index(request):
     if request.method == 'POST':
+        color = request.POST["color"]
+        try:
+            color = ImageColor.getcolor(color, 'RGB')
+        except ValueError:
+            color = '#000000'
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             uploaded_file_in_memory = request.FILES["image"].read()
@@ -16,8 +22,11 @@ def index(request):
             img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
             black = np.sum(img == 0)
             white = np.sum(img == 255)
+            pixel_count = np.sum(img == color)
             pixel = 'черных' if black > white else 'белых'
-            return render(request, 'index.html', {'form': form, 'img_obj': img_obj, 'pixel': pixel})
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj,
+                                                  'pixel': pixel, 'pixel_count': pixel_count,
+                                                  'color': color})
     else:
         form = ImageForm()
     return render(request, 'index.html', {'form': form})
