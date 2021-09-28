e1a92a8d430836b066859d5a1ed2db7c0d69c510
Rand0mizator <iosuslov@gmail.com>
2021-09-28 16:02:17 +0700

    Counting pixels

--- ml_python/app/views.py
+++ ml_python/app/views.py
@@ -9,12 +9,15 @@ def index(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
+            uploaded_file_in_memory = request.FILES["image"].read()
             form.save()
             img_obj = form.instance
-            # img = cv2.imread(img_obj)
-            # black = np.sum(img == 0)
-            # white = np.sum(img == 255)
-            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+            nparr = np.fromstring(uploaded_file_in_memory, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+            black = np.sum(img == 0)
+            white = np.sum(img == 255)
+            pixel = 'черных' if black > white else 'белых'
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj, 'pixel': pixel})
     else:
         form = ImageForm()
     return render(request, 'index.html', {'form': form})
